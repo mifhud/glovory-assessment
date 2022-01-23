cf92f8d9968a5afafe383e766c266f6a9fbb60b5
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 21:37:33 +0700

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { setupSwagger } from './docs-swagger';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
   const serverConfig = await config.get('server');
+
+  setupSwagger(app);
 
   const host = process.env.SERVER_HOST || serverConfig.host;
   const port = process.env.SERVER_PORT || serverConfig.port;
