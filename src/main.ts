0509825485b4003d8e3e7fcac3a9919f88602674
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 23:46:34 +0700

    setup grpc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { setupSwagger } from './docs-swagger';
+import { join } from 'path';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const grpcConfig = await config.get('grpc');
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'glovory',
+      protoPath: ['address.proto', 'auth.proto'],
+      loader: {
+        includeDirs: [join(__dirname, 'protos')],
+      },
+      url: `${grpcConfig.host}:${grpcConfig.port}`,
+    },
+  });
+
+  await app.startAllMicroservices();
+  logger.log(`GRPC listening on ${grpcConfig.host}:${grpcConfig.port}`);
+
   await app.setGlobalPrefix('/api/v1');
 
   const serverConfig = await config.get('server');
