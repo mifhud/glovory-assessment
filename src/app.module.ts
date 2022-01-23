79552deca07e8d0cd152bacddad65275a98d1b2e
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 23:04:03 +0700

    import auth module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import * as typeOrmConfig from './database/config/ormconfig';
 import { AddressesModule } from './models/addresses/addresses.module';
 import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AddressesModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UsersModule,
+    AddressesModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
