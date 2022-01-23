5dce62a69d296ebcd2e5dc0e5666336c2d049781
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 20:26:18 +0700

    applying configuration and logging when init bootstrap application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const serverConfig = await config.get('server');
+
+  const host = process.env.SERVER_HOST || serverConfig.host;
+  const port = process.env.SERVER_PORT || serverConfig.port;
+
+  await app.listen(port, host);
+  logger.log(`Application listening on ${host} port ${port}`);
 }
 bootstrap();
