73b88c364a2c344bf1e2d952e970b1de4550c9f4
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 22:23:25 +0700

    import address module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as typeOrmConfig from './database/config/ormconfig';
+import { AddressesModule } from './models/addresses/addresses.module';
 import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AddressesModule],
   controllers: [],
   providers: [],
 })
