e47d24cbae38dd6648e08b017fc4bdc9bfd8bfde
Miftahul Huda <miftahulxhuda@gmail.com>
2022-01-23 22:00:41 +0700

    import user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as typeOrmConfig from './database/config/ormconfig';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
   controllers: [],
   providers: [],
 })
